7375453c957b5e28cc891687308b83a1e07be846
Kenneth Kousen <ken.kousen@kousenit.com>
2012-09-11 07:46:14 -0600

    making groovy 1.8 default

--- src/main/groovy/ast/tostring/tostring_and_equals.groovy
+++ src/main/groovy/ast/tostring/tostring_and_equals.groovy
@@ -15,11 +15,15 @@
  * ========================================================== */
 package ast.tostring
 
+import groovy.transform.Canonical;
 import groovy.transform.EqualsAndHashCode;
 import groovy.transform.ToString;
+import groovy.transform.TupleConstructor;
 
-@EqualsAndHashCode
-@ToString
+//@EqualsAndHashCode
+//@ToString
+//@TupleConstructor
+@Canonical
 class Product {
     int id
     String name
@@ -29,11 +33,12 @@ class Product {
 Product p_orig = new Product(id:1,name:'name',price:9.99)
 Product p_copy = new Product(id:1,name:'name',price:9.99)
 Product p_diff = new Product(id:2,name:'other',price:5)
+Product p2 = new Product(3, 'abc', 10)
 
 assert p_orig == p_copy
 assert !p_orig.is(p_copy)
 assert p_orig != p_diff
 assert p_orig.toString() == 'ast.tostring.Product(1, name, 9.99)'
 
-Set<Product> products = [p_orig, p_copy, p_diff] as Set
+Set<Product> products = [p_orig, p_copy, p_diff]
 assert products.size() == 2

540e8184596ff355ae2d5cfaf47212e309391b6a
Kenneth Kousen <ken.kousen@kousenit.com>
2015-12-14 15:40:46 -0600

    Fix cat pictures to search for cats

--- src/main/groovy/flickr/cat_pictures.groovy
+++ src/main/groovy/flickr/cat_pictures.groovy
@@ -12,7 +12,7 @@ String endPoint = 'https://api.flickr.com/services/rest?'
 def params = [method: 'flickr.photos.search',
               api_key: key,
               format: 'json',
-              tags: 'SpaceHistory',
+              tags: 'cat',
               nojsoncallback: 1,
               media: 'photos',
               per_page: 6]

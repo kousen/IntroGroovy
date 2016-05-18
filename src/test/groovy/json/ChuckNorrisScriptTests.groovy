f01130b67525e2fa4ae354a727a7a74f228f7382
Kenneth Kousen <ken.kousen@kousenit.com>
2016-05-17 23:40:53 -0500

    Remove online chuck norris script test -- site too inconsistent

--- src/test/groovy/json/ChuckNorrisScriptTests.groovy
+++ src/test/groovy/json/ChuckNorrisScriptTests.groovy
@@ -22,18 +22,18 @@ import groovy.mock.interceptor.StubFor
 class ChuckNorrisScriptTests {
     String base = 'src/main/groovy'
 
-    @Test
-    void testChuckNorrisOnline() {
-        GroovyShell shell = new GroovyShell()
-        shell.evaluate(new File("$base/json/chuck_norris.groovy"))
-    }
+//    @Test
+//    void testChuckNorrisOnline() {
+//        GroovyShell shell = new GroovyShell()
+//        shell.evaluate(new File("$base/json/chuck_norris.groovy"))
+//    }
     
     @Test
     void testChuckNorrisOffline() {
         def result = '''
         {
             "value" : {
-                "joke" : "Chuck Norris can make a class abstract AND final"
+                "joke" : "Chuck Norris can make a method abstract AND final"
             }
         }
         '''

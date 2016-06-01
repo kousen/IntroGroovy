b771c8c29df9ab18fc3823abd4882c8d274ec2ef
Kenneth Kousen <ken.kousen@kousenit.com>
2016-06-01 13:49:51 +0200

    Add LocalDate demo with metaprogramming

---
+++ src/main/groovy/metaprogramming/LocalDate_demo.groovy
@@ -0,0 +1,13 @@
+package metaprogramming
+
+import java.time.*
+import java.time.temporal.ChronoUnit
+
+LocalDate.metaClass.minus = { LocalDate date ->
+    date.until(delegate, ChronoUnit.DAYS)
+}
+
+LocalDate electionDay = LocalDate.of(2016, Month.NOVEMBER, 8)
+LocalDate today = LocalDate.now()
+
+println "${electionDay - today} days to go"

028924282f61a984e877809bf864463acfd17281
Ken Kousen <ken.kousen@kousenit.com>
2023-02-04 18:21:54 -0500

    Add Paul Kings solution and did a static import of Month

--- src/main/groovy/datetime/groundhogday.groovy
+++ src/main/groovy/datetime/groundhogday.groovy
@@ -3,18 +3,19 @@ package datetime
 import groovy.time.TimeCategory
 
 import java.time.LocalDate
-import java.time.Month
 import java.time.temporal.ChronoUnit
 
-def ghday = LocalDate.of(2023, Month.FEBRUARY, 2)
-def spring = LocalDate.of(2023, Month.MARCH, 20)
+import static java.time.Month.*
+
+def ghday = LocalDate.of(2023, FEBRUARY, 2)
+def spring = LocalDate.of(2023, MARCH, 20)
 long days = spring - ghday
 long weeks = ghday.until(spring, ChronoUnit.WEEKS)
 println "There are $days between Groundhog Day and the 1st day of Spring"
 println "Thats $weeks weeks and ${days - 7 * weeks} days"
 
 // From Paul King (see https://groovy.apache.org/blog/groundhog-day):
-var groundHogDay =  LocalDate.of(2023, Month.FEBRUARY, 2)
-var springEquinoxNorthernHemisphere = LocalDate.of(2023, Month.MARCH, 20)
+var groundHogDay =  LocalDate.of(2023, FEBRUARY, 2)
+var springEquinoxNorthernHemisphere = LocalDate.of(2023, MARCH, 20)
 var expected = use(TimeCategory) { 6.weeks + 4.days }.days
 assert springEquinoxNorthernHemisphere - groundHogDay == expected
